08ae5750f3dead6e18ab7537e140f2520ae72c45
Mojammal Hock <Mojammal.Hock@adesso.de>
2019-06-21 18:51:53 +0200

    some sample script

--- src/test/resources/convertInitDbscript.sh
+++ src/test/resources/convertInitDbscript.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-#sed -i.bak 's/ FOR SBCS DATA WITH DEFAULT NULL//g' initDB.sql 
-#sed -i.bak 's/ FOR SBCS DATA//g' initDB.sql 
-#sed -i.bak 's/ WITH DEFAULT NULL//g' initDB.sql
-#sed -i.bak 's/#/_/g' initDB.sql
+#sed -i.bak 's/ FOR SBCS DATA WITH DEFAULT NULL//g' create_db_2.sql
+#sed -i.bak 's/ FOR SBCS DATA//g' create_db_2.sql
+#sed -i.bak 's/ WITH DEFAULT NULL//g' create_db_2.sql
+#sed -i.bak 's/#/_/g' create_db_2.sql
 
 #while read -r line; do
 #  echo "one: $line"
 #  echo ""
-#done < ((sed -n '/CREATE TABLE/,/;/p' initDB.sql))
+#done < ((sed -n '/CREATE TABLE/,/;/p' create_db_2.sql))
 
 lines=$(sed '/CREATE TABLE/,/;/!d' initDB.sql)
 
