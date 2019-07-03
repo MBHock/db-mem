5ff6788663d55dfbf659ee9cac2eb17517881b8b
Mojammal Hock <Mojammal.Hock@adesso.de>
2019-07-03 14:20:20 +0200

    a main method for some test

--- src/main/java/org/mhb/db/SqlStatementReader.java
+++ src/main/java/org/mhb/db/SqlStatementReader.java
@@ -2,9 +2,9 @@ package org.mhb.db;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SqlStatementReader implements StatementReader {
 
@@ -30,5 +30,30 @@ public class SqlStatementReader implements StatementReader {
 
             throw new RuntimeException(e);
         }
+    }
+
+    public static void main(String... strings) {
+        int min = 1;
+        int max = 2000;
+        Random random = new Random();
+
+        Integer[] intArr = new Integer[1500];
+        for (int i = 0; i < 1500; i++) {
+            intArr[i] = random.nextInt((max - min) + 1) + min;
+        }
+
+        Arrays.sort(intArr);
+
+        //Stream.of(intArr);
+        //int nextInt = random.nextInt((max-min)+1 ) +min;
+        //List<Integer> integers = IntStream.range(0, 20000).mapToObj(Integer::valueOf).collect(Collectors.toList());
+        //System.out.println(integers);
+        //String str = integers.stream().map(String::valueOf).collect(Collectors.joining(", "));
+        //String str = integers.parallelStream().map(String::valueOf).collect(Collectors.joining(", "));
+        System.out.println("After sort");
+        System.out.println(Arrays.toString(intArr));
+
+        List<Integer> fromStream = Stream.of(intArr).collect(Collectors.toList());
+        System.out.println("Passed through: " + fromStream);
     }
 }
